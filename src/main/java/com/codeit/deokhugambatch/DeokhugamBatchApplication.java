c1ebc904b459dcc671496945b491fdcd44ef0dca
Hize18 <98449664+Hize18@users.noreply.github.com>
2026-06-30 16:46:49 +0900

    feat: 알림 삭제 스케쥴러 구현

--- src/main/java/com/codeit/deokhugambatch/DeokhugamBatchApplication.java
+++ src/main/java/com/codeit/deokhugambatch/DeokhugamBatchApplication.java
@@ -2,12 +2,14 @@ package com.codeit.deokhugambatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class DeokhugamBatchApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DeokhugamBatchApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DeokhugamBatchApplication.class, args);
+  }
 
 }
