2d8a6e1736118da495e6f0a81b5583ba0fde4c02
Hize18 <98449664+Hize18@users.noreply.github.com>
2026-07-02 13:36:55 +0900

    chore: 공통 패키지 의존성 및 env 예시 추가

--- src/main/java/com/codeit/deokhugambatch/DeokhugamBatchApplication.java
+++ src/main/java/com/codeit/deokhugambatch/DeokhugamBatchApplication.java
@@ -2,8 +2,10 @@ package com.codeit.deokhugambatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan("com.codeit.deokhugamcommon.domain")
 public class DeokhugamBatchApplication {
 
 	public static void main(String[] args) {
